de86851b9e94851083ddc6ef2d8c0a23dbc4f7e9
Abdelmajid El-oualy <abdel-ou@c1r8p8.1337.ma>
2025-08-02 12:57:35 +0100

    add LoadBalancer

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-k3d cluster create mycluster
+k3d cluster create mycluster --port "8888:8888@loadbalancer"
 kubectl create namespace argocd
 kubectl create namespace dev
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
