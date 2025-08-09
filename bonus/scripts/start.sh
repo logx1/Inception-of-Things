7e7638ee2cae389854f6deea8a3dbf2010145dd5
Abdelmajid El-oualy <abdel-ou@c1r8p8.1337.ma>
2025-08-09 12:57:02 +0100

    remove pod.yml

---
+++ bonus/scripts/start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+k3d cluster create mycluster --port "8888:8888@loadbalancer"
+kubectl create namespace argocd
+kubectl create namespace dev
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# # Wait for ArgoCD to be ready
+kubectl wait --for=condition=available --timeout=60s deployment/argocd-server -n argocd
+
+USERNAME=admin  # Fixed variable assignment
+PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)  # Fixed variable assignment
+
+kubectl port-forward -n argocd svc/argocd-server 8080:443 &
+
+sleep 5  # Wait for port-forwarding to establish
+
+# # LOGIN TO ARGOCD
+argocd login localhost:8080 --username admin --password "$PASSWORD" --insecure
+
+argocd app create my-app \
+  --repo https://github.com/logx1/Inception-of-Things.git \
+  --path p3/app-conf \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace dev \
+  --sync-policy automated \
+  --auto-prune \
+  --self-heal
+
+echo "ArgoCD Password: $PASSWORD"
