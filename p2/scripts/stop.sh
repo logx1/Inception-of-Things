9f8328c40ccdb1a79c1db9b753aa12252468cc5f
Abdelmajid El-oualy <abdel-ou@c1r8p1.1337.ma>
2025-08-16 19:49:56 +0100

    fix  directory structure for the p2

--- p2/scripts/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo k3s kubectl delete -f /vagrant/app1.yaml
-sudo k3s kubectl delete -f /vagrant/app2.yaml
-sudo k3s kubectl delete -f /vagrant/app3.yaml
-sudo k3s kubectl delete -f /vagrant/ingress.yaml
